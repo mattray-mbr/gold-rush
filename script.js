dd8cfb94535eda33250cad45f6437966737b5199
Matt Ray <mattray.mbr@gmail.com>
2016-01-26 15:09:32 -0700

    fixed hopefully

---
+++ script.js
@@ -0,0 +1,54 @@
+angular.module('myApp', []);
+
+angular.module('myApp')
+    .controller('myController', ['$scope', function($scope){
+        
+			$scope.coordinates = [];
+            $scope.list = [];
+            $scope.popForm = 'comment';
+			$scope.doClick = function($event) {
+                
+//              this logs coordiantes only
+//				var coords = {'x': 0, 'y': 0};
+//				coords['x'] = $event.pageX;
+//				coords['y'] = $event.pageY;
+//              $scope.coordinates.push(coords);
+//				console.log($scope.coordinates);
+                
+//              .spot with client or pageX top left
+                // $scope.popUp = false;
+		        $scope.button = "Mark Comments";
+		        $scope.showPop = function($event) {
+			    $scope.popUp = !$scope.popUp
+		      }
+                
+                $scope.spot = {  
+                'top': $event.pageY + "px",
+				'left': $event.pageX + "px",
+			     }
+                $scope.coordinates.push($scope.spot);
+            
+                console.log($scope.coordinates);
+                console.log($event);
+                console.log($scope.spot);
+            
+                
+                $scope.removeIt = function($index){
+                $scope.coordinates.splice($index, 1);
+                }
+            }
+
+            $scope.submit = function(){
+                if($scope.popForm) {
+                    $scope.list.push($scope.popForm);
+                    $scope.popForm = ''
+                    console.log($scope.list)
+                    $scope.showPop = false;
+                }
+            }
+            
+    
+}])
+
+
+      
